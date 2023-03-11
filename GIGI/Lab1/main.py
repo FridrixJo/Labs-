4ff16ad6659afef7f3f4cd58bcfd2389a5fc1102
fridrixjo <pglutov@gmail.com>
2023-03-11 17:50:40 +0300

    add function

--- GIGI/Lab1/main.py
+++ GIGI/Lab1/main.py
@@ -1,2 +1,39 @@
 print('hola')
 
+
+def parametrized_wrapper(*args):
+    def wrapper(func: callable):
+        def inner():
+            print('start')
+            func()
+            print('end')
+            for i in args:
+                print(i)
+        return inner
+    return wrapper
+
+
+@parametrized_wrapper(1, 23, "ofijihfruijfr")
+def my_func():
+    print('say hola')
+
+
+my_func()
+
+def make_operation(n, m, operation):
+    match operation:
+        case "add":
+            return n + m
+        case "sub":
+            return n - m
+        case "mul":
+            return n * m
+        case "div":
+            return n / m
+
+
+operation = input("input data .. ")
+
+print(make_operation(13, 4, operation))
+
+
