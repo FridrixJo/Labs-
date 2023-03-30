f3d667f73e2bd6c7c982de253217fb9ce169307e
fridrixjo <pglutov@gmail.com>
2023-03-30 17:21:40 +0300

    implement container methods

--- GIGI/Lab2/task1/main.py
+++ GIGI/Lab2/task1/main.py
@@ -1,14 +1,19 @@
 from parser import Parser
 
-with open('file.txt', 'r') as file:
-    text = file.read()
 
-parser = Parser(text)
-print(parser.count_sentences())
-print(parser.count_non_declarative_sentences())
-print(parser.count_words())
-print(parser.get_all_words())
-print(parser.get_numbers())
-print(parser.get_words())
-print(parser.count_average_length())
-print(parser.top_K_related_N_grams(3, 2))
+def main():
+    with open('file.txt', 'r') as file:
+        text = file.read()
+    parser = Parser(text)
+
+    print("count sentences:", parser.count_sentences())
+    print("count non declarative", parser.count_non_declarative_sentences())
+    print("count words", parser.count_words())
+    print("count numbers", parser.get_numbers())
+    print("count words", parser.get_words())
+    print("count averange length", parser.count_average_length())
+    print("top K related N grams", parser.top_K_related_N_grams(3, 2))
+
+
+if __name__ == '__main__':
+    main()
