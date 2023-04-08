d032c3e5ef513a804961d7f042c8a8a1e5c5d354
fridrixjo <pglutov@gmail.com>
2023-04-08 10:09:28 +0300

    fix menu

--- GIGI/Lab2/task2/container.py
+++ GIGI/Lab2/task2/container.py
@@ -3,21 +3,31 @@ import re
 
 class Container:
 
-    def __init__(self):
-        self.users = ['pavel']
-        self.current_user = 'pavel'
-        self.storage: dict[str, set] = {'pavel': set()}
+    def __init__(self, user: str = 'pavel'):
+        self.users = {user,}
+        self.current_user = user
+        self.storage: dict[str, set] = {user: set()}
 
     def add(self, *keys):
-        try:
-            for key in keys:
-                self.storage[self.current_user].add(key)
-        except Exception:
-            print('wrong keys')
+        if self.current_user:
+            try:
+                for key in keys:
+                    self.storage[self.current_user].add(key)
+            except Exception:
+                print('wrong keys')
+        else:
+            print('add user')
 
     def remove(self, key):
         try:
-            self.storage[self.current_user].discard(key)
+            if self.storage[key]:
+                del self.storage[key]
+                self.users.discard(key)
+                if key == self.current_user:
+                    if len(self.users):
+                        self.current_user = random.choice(list(self.users))
+                    else:
+                        self.current_user = None
         except Exception:
             print('wrong key')
 
@@ -25,37 +35,58 @@ class Container:
         for key, value in self.storage.items():
             print(key, ':', value)
 
-    def find(self, keys):
-        try:
-            print(self.storage[self.current_user].intersection(set(keys)))
-        except Exception:
-            print('wrong keys')
+    def find(self, *keys):
+        if self.current_user:
+            try:
+                for key in keys:
+                    if self.storage[self.current_user].intersection({key,}):
+                        print(f'key: {key} exists')
+                    else:
+                        print(f'key: {key} does not exist')
+            except Exception:
+                print('wrong keys')
+        else:
+            print('add user')
 
     def grep(self, regex):
-        status = False
-        for i in self.storage[self.current_user]:
-            if re.search(regex, i):
-                print(i)
-        if status is False:
-            print('Not found')
+        if self.current_user:
+            status = False
+            for i in self.storage[self.current_user]:
+                if re.search(regex, i):
+                    print(i)
+            if status is False:
+                print('Not found')
+        else:
+            print('add user')
 
     def save(self):
-        with open('container.txt') as file:
+        with open('container.txt', 'w') as file:
             for i in self.users:
-                file.write(f'{i}: {self.storage[i]} \n')
+                file.write(f'{i} {" ".join(str(i) for i in self.storage[i])}\n')
 
 
-    def switch(self, new_user: str=None):
+    def load(self):
+        with open('container.txt', 'r') as file:
+            for line in file:
+                data_arr = line.replace('\n', "").split(' ')
+                self.users.add(data_arr[0])
+                self.current_user = data_arr[0]
+                self.storage[data_arr[0]] = set(data_arr[1:])
+
+
+    def switch(self, new_user: str = None):
         if new_user:
             self.current_user = new_user
             self.storage[new_user] = set()
-            self.users.append(new_user)
+            self.users.add(new_user)
         else:
             if len(self.users):
-                self.current_user = random.choice(self.users)
+                self.current_user = random.choice(list(self.users))
+            else:
+                self.current_user = None
 
 
-    # def load(self):
+
 
 
 
--- GIGI/Lab2/task2/main.py
+++ GIGI/Lab2/task2/main.py
@@ -1,9 +1,79 @@
+import random
+
 print('hola')
 from container import Container
+#
+# a = Container()
+# a.load()
+# a.list()
+# a.add(1, 45, 5, 1, 45)
+# a.list()
+# a.switch('ilya')
+# a.list()
+# a.save()
+# a.load()
 
-a = Container()
-a.add(1, 45, 5, 1, 45)
-a.list()
-a.switch('ilya')
-a.switch('ilya')
-a.list()
+# a = Container()
+# a.load()
+# print(a.current_user)
+# a.switch()
+# print(a.current_user)
+# a.add(54, 'fdihjfi')
+# a.list()
+# a.find(54, 22)
+# a.remove('andrew')
+# a.list()
+# a.save()
+a = random.choice(list(set()))
+print(a)
+
+# a = set()
+# a.add('hola')
+# a.add('slaa')
+# for i in a:
+#     print(i)
+
+
+def main_menu():
+    print('0 - exit')
+    print('1 - add')
+    print('2 - remove')
+    print('3 - find')
+    print('4 - list')
+    print('5 - grep')
+    print('6 - save')
+    print('7 - load')
+    print('8 - switch')
+
+def main():
+    username = input('Enter username: ')
+    while not username:
+        username = input('Enter username: ')
+
+    container = Container(username)
+
+    while True:
+        n = 0
+        try:
+            n = int(input('enter n: '))
+        except Exception:
+            pass
+
+        if n == 0:
+            break
+        elif n == 1:
+            data = (input("enter data to add: ")).split(" ")
+            container.add(*data)
+        elif n == 2:
+            key = str(input("enter key to delete"))
+            container.remove(key)
+        elif n == 3:
+            data = (input("enter data to add: ")).split(" ")
+            container.find(*data)
+        elif n == 4:
+            container.list()
+        elif n == 5:
+
+
+
+
