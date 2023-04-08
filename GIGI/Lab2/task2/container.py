9f05de3ca726cd9a978629b2e7b43274bc7ec2e5
fridrixjo <pglutov@gmail.com>
2023-04-08 08:19:46 +0300

    add switching users

--- GIGI/Lab2/task2/container.py
+++ GIGI/Lab2/task2/container.py
@@ -1,36 +1,39 @@
+import random
 import re
 
 class Container:
 
     def __init__(self):
-        self.name = ''
-        self.storage: dict[str, set] = {}
+        self.users = ['pavel']
+        self.current_user = 'pavel'
+        self.storage: dict[str, set] = {'pavel': set()}
 
-    def __add__(self, keys: list):
+    def add(self, *keys):
         try:
             for key in keys:
-                self.storage[self.name].add(key)
+                self.storage[self.current_user].add(key)
         except Exception:
             print('wrong keys')
 
     def remove(self, key):
         try:
-            self.storage[self.name].discard(key)
+            self.storage[self.current_user].discard(key)
         except Exception:
             print('wrong key')
 
     def list(self):
-        print(self.storage[self.name])
+        for key, value in self.storage.items():
+            print(key, ':', value)
 
     def find(self, keys):
         try:
-            print(self.storage[self.name].intersection(set(keys)))
+            print(self.storage[self.current_user].intersection(set(keys)))
         except Exception:
             print('wrong keys')
 
     def grep(self, regex):
         status = False
-        for i in self.storage[self.name]:
+        for i in self.storage[self.current_user]:
             if re.search(regex, i):
                 print(i)
         if status is False:
@@ -38,9 +41,21 @@ class Container:
 
     def save(self):
         with open('container.txt') as file:
-            for i in self.
+            for i in self.users:
+                file.write(f'{i}: {self.storage[i]} \n')
 
-    def load(self):
+
+    def switch(self, new_user: str=None):
+        if new_user:
+            self.current_user = new_user
+            self.storage[new_user] = set()
+            self.users.append(new_user)
+        else:
+            if len(self.users):
+                self.current_user = random.choice(self.users)
+
+
+    # def load(self):
 
 
 
--- GIGI/Lab2/task2/main.py
+++ GIGI/Lab2/task2/main.py
@@ -1,1 +1,9 @@
 print('hola')
+from container import Container
+
+a = Container()
+a.add(1, 45, 5, 1, 45)
+a.list()
+a.switch('ilya')
+a.switch('ilya')
+a.list()
